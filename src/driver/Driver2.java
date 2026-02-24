dfcdce52b60310b44cd12261d0b76450ef84d44e
IndahSiahaan <triyunisiahaan@gmail.com>
2026-02-24 13:57:45 +0700

    Update Driver2.java

--- src/driver/Driver2.java
+++ src/driver/Driver2.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class Driver2 {
-    
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+        int[] nilai = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            nilai[i] = input.nextInt();
+        }
+
+        int total = 0;
+
+        // Jumlahkan semua nilai
+        for (int i = 0; i < N; i++) {
+            total += nilai[i];
+        }
+
+        System.out.println(total);
+
+        input.close();
+    }
 }
